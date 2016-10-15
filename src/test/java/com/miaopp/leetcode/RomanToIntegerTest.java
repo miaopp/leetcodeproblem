a8488b71156df5358bb25f35ab71c5e291a0114e
miaoping <miaoping@lianjia.com>
2016-10-15 21:44:02 +0800

    commit problem roman to integer

---
+++ src/test/java/com/miaopp/leetcode/RomanToIntegerTest.java
@@ -0,0 +1,13 @@
+package com.miaopp.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Created by miaoping on 16/10/15.
+ */
+public class RomanToIntegerTest {
+    @Test
+    public void run() {
+        System.out.println(new RomanToInteger().romanToInt("CD"));
+    }
+}
